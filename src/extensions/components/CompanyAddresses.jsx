df73384d1233dbd446e0e855f827f1e537771d64
pranaywalia <pranaywalia@gmail.com>
2025-05-20 16:50:33 +0200

    Update CompanyAddresses.jsx

--- src/extensions/components/CompanyAddresses.jsx
+++ src/extensions/components/CompanyAddresses.jsx
@@ -1,35 +1,12 @@
 import React from 'react';
-import { Card, Text, Grid, Col } from '@hubspot/ui-extensions';
 
-export default function CompanyAddresses({ context }) {
-  const { object } = context;
-  const properties = object.properties;
-
-  function renderAddress(prefix) {
-    return (
-      <div style={{ marginBottom: '12px' }}>
-        <Text as="h5">{prefix.replace('_', ' ').toUpperCase()}</Text>
-        <Grid columns={1}>
-          <Col>
-            <Text>Street: {properties[prefix + '_street'] || 'N/A'}</Text>
-            <Text>City: {properties[prefix + '_city'] || 'N/A'}</Text>
-            <Text>State: {properties[prefix + '_state'] || 'N/A'}</Text>
-            <Text>Zip: {properties[prefix + '_zip'] || 'N/A'}</Text>
-            <Text>Country: {properties[prefix + '_country'] || 'N/A'}</Text>
-          </Col>
-        </Grid>
-      </div>
-    );
-  }
-
+const companyAddressesModule = () => {
   return (
-    <Card>
-      {renderAddress('billing_address_1')}
-      {renderAddress('billing_address_2')}
-      {renderAddress('billing_address_3')}
-      {renderAddress('shipping_address_1')}
-      {renderAddress('shipping_address_2')}
-      {renderAddress('shipping_address_3')}
-    </Card>
+    <div>
+      <h2>Company Addresses</h2>
+      {/* Your UI here */}
+    </div>
   );
-}
+};
+
+export default companyAddressesModule;
