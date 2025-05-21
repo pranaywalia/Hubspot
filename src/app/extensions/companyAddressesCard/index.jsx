ee9e9fc7b006fe31f29c315d8ae5d67288a83ba3
pranaywalia <pranaywalia@gmail.com>
2025-05-21 11:14:10 +0200

    Update index.jsx

--- src/app/extensions/companyAddressesCard/index.jsx
+++ src/app/extensions/companyAddressesCard/index.jsx
@@ -1,29 +1,7 @@
 import React from 'react';
-import {
-  Heading,
-  Button,
-  Text,
-  Flex,
-  hubspot,
-} from '@hubspot/ui-extensions';
-import { CompanyAddresses } from './components/CompanyAddresses';
+import { hubspot } from '@hubspot/ui-extensions';
+import { CompanyAddressesCard } from './components/CompanyAddressesCard';
 
-hubspot.extend(({ actions, context }) => (
-  <CompanyAddressesCard actions={actions} context={context} />
+hubspot.extend(({ context, actions }) => (
+  <CompanyAddressesCard context={context} actions={actions} />
 ));
-
-const CompanyAddressesCard = ({ actions, context }) => {
-  return (
-    <Flex direction="column" gap="small">
-      <Heading>Company Addresses</Heading>
-      <Text>Edit the addresses associated with this company.</Text>
-
-      <Button
-        variant="primary"
-        overlay={<CompanyAddresses actions={actions} context={context} />}
-      >
-        Edit Addresses
-      </Button>
-    </Flex>
-  );
-};
