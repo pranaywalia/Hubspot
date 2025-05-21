78439a6f1ed0a5538a9411a4f2ed34e8d0cb2cbd
pranaywalia <pranaywalia@gmail.com>
2025-05-21 10:47:55 +0200

    Add files via upload

---
+++ src/app/extensions/companyAddressesCard/components/CompanyAddresses.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CompanyAddresses() {
+  return (
+    <div style={{ padding: '10px' }}>
+      <h3>Company Addresses</h3>
+      <p>This card will show company address details here.</p>
+    </div>
+  );
+}
---
+++ src/app/extensions/companyAddressesCard/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CompanyAddresses from './components/CompanyAddresses';
+
+export default function CompanyAddressesCard(props) {
+  // You can access record data via props
+
+  return <CompanyAddresses />;
+}
