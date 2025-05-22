6ccea811388cda6650bd8502fa87cfc50342f667
pranaywalia <pranaywalia@gmail.com>
2025-05-22 09:10:09 +0200

    Delete src/app directory

--- src/app/extensions/companyAddressesCard/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { hubspot } from '@hubspot/ui-extensions';
-import { CompanyAddressesCard } from './components/CompanyAddressesCard';
-
-hubspot.extend(({ context, actions }) => (
-  <CompanyAddressesCard context={context} actions={actions} />
-));
